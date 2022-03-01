383463f8bd6b377cc018172852fee74b11644b4b
RaoniPirat <raoni.pirat@gmail.com>
2022-03-01 10:50:53 +0100

    users added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#User.create(first_name: 'Sara', last_name: 'Clark', email: 'test@gmail.com', encrypted_password: '123456')
+#User.create(first_name: "Med", last_name: "Itation", email: "test2@gmail.com", encrypted_password: "123456")
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users..."
+sara = { first_name: 'Sara', last_name: 'Clark', email: 'test@gmail.com', password: '123456' }
+med =  { first_name: "Med", last_name: "Itation", email: "test2@gmail.com", password: "123456" }
+
+[ sara, med ].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.first_name}"
+end
+puts "Finished!"
