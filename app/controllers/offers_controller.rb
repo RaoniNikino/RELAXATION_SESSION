de7cba4e19457ccad20a2ac91da37dcd5c130972
RaoniPirat <raoni.pirat@gmail.com>
2022-03-04 11:05:14 +0100

    some work

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,16 +5,16 @@ class OffersController < ApplicationController
     @offers = policy_scope(Offer)
     if params[:category]
       @offers = Offer.where(category: params[:category])
-      elsif params[:query].present?
-              sql_query = " \
+    elsif params[:query].present?
+      sql_query = " \
         offers.name ILIKE :query \
         OR users.first_name ILIKE :query \
         OR users.last_name ILIKE :query \
+        Or offers.address ILIKE :query \
       "
       @offers = Offer.joins(:user).where(sql_query, query: "%#{params[:query]}%")
-
-      else
-        @offers = Offer.all
+    else
+      @offers = Offer.all
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_153211) do
+ActiveRecord::Schema.define(version: 2022_03_04_083913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,11 +61,11 @@ ActiveRecord::Schema.define(version: 2022_03_03_153211) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "description"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.string "photo"
+    t.text "description"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
